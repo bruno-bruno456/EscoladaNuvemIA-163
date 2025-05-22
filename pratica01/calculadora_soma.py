c17f8d512191b8d8170d80838bd61b5b0025e1dd
Bruno <bruno.marques@escoladanuvem.org>
2025-05-22 11:24:33 -0300

    Prática 01

---
+++ pratica01/calculadora_soma.py
@@ -0,0 +1,18 @@
+""" (docs strings)
+Calculadora de Soma
+Desenvolva um programa que some dois números. 
+Use as variáveis numero1 = 12 e numero2 = 14. 
+O programa deve calcular a soma e exibir o resultado.
+"""
+
+# Este é um programa que soma dois números (comment strings)
+
+# Variáveis
+numero1 = 12
+numero2 = 14
+
+# Calcula a soma de dois número
+soma = numero1 + numero2
+
+# Exibir o resultado
+print(f"A soma de {numero1} e {numero2} é {soma}") #f strings
---
+++ pratica01/calculadora_volume.py
@@ -0,0 +1,21 @@
+"""
+Calculadora de Volume
+- Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+- Comprimento: 12 cm
+- Largura: 14 cm
+- Altura: 20 cm
+O programa deve calcular o volume e exibir o resultado em cm³.
+"""
+
+# Calculo do volume de uma caixa retangular
+
+#Variáveis
+comprimento = 12
+largura = 14
+altura = 20
+
+# Calcular o volume da caixa
+volume = comprimento * largura * altura
+
+# Exibindo o resultado
+print(f"O volume da caixa é {volume} cm³")
